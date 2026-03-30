aa0a5a248879da534ba9f8d2f31fa8733230b0e8
tavalentine00 <tavalentinecs@gmail.com>
2026-03-29 22:24:53 -0700

    Add scheduler to getWikipediaSummary action

--- convex/chat.ts
+++ convex/chat.ts
@@ -1,12 +1,12 @@
-import { mutation, query } from "./_generated/server";
+import { internalAction, mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { api, internal } from "./_generated/api";
 
 export const getMessages = query({
   args: {},
   handler: async (ctx) => {
     const messages = await ctx.db.query("messages").order("desc").take(50);
     return messages.reverse();
-    // return await ctx.db.query("messages").order("asc").collect();
   },
 });
 
@@ -24,17 +24,32 @@ export const sendMessage = mutation({
       poop: args.poop,
     });
 
-    if(args.body.startsWith("/wiki")) {
-      const topic = args.body.slice(args.body.indexOf("") + 1);
-      const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=extracts&exintro&explaintext&titles=${topic}`);
-      const data = await response.json();
-      const firstPageId = Object.keys(data.query.pages)[0];
-      const summary = data.query.pages[firstPageId].extract;
-      await ctx.db.insert("messages", {
-        user: "Wikipedia",
-        body: summary,
-        poop: "wikipedia",
-      });
-    }
+    if (args.body.startsWith("/wiki")) {
+      const topic = args.body.slice(args.body.indexOf(" ") + 1);
+      await ctx.scheduler.runAfter(0, internal.chat.getWikipediaSummary, { topic });
+    }  
+  },  
+});
+
+export const getWikipediaSummary = internalAction({
+  args: { topic: v.string() },
+  handler: async (ctx, args) => {
+    const response = await fetch(
+      `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=extracts&exintro&explaintext&titles=${args.topic}`
+    );
+
+    const summary = getSummaryFromJSON(await response.json());
+
+    await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+
+      user: "Wikipedia",
+      body: summary,
+      poop: "wikipedia",
+    });
   },
 });
+
+function getSummaryFromJSON(data: any) {
+  const firstPageId = Object.keys(data.query.pages)[0];
+  return data.query.pages[firstPageId].extract;
+}
